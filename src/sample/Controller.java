747fb1f5f1b51ce8733459061fbd07cc2d1b94c6
Markus Gärtner <gaertner.brilon@gmail.com>
2019-09-03 15:38:19 +0200

    20190903 15:38

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,20 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.IOException;
 
 public class Controller {
-    public void handleButtonActionNewProject(ActionEvent actionEvent) {
+    public void handleButtonActionNewProject(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("NewProject.fxml"));
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Neues Projekt");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
