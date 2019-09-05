9660e98ff1f2a7c147607efe4857f0f894d3e89f
Markus Gärtner <gaertner.brilon@gmail.com>
2019-09-05 10:56:44 +0200

    20190905 10:56

--- src/sample/NewProject.java
+++ src/sample/NewProject.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class NewProject {
+    public void handleButtonActionNext(ActionEvent actionEvent) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("NewDescription.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Neues Projekt");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
 }
